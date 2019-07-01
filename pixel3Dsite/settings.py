fdc8b813d0349651469780fdbe6d430c56ed461d
Paul Breugnot <paul.breugnot@etu.emse.fr>
2019-07-01 18:47:31 +0200

    Set up ATOMIC_REQUESTS

--- pixel3Dsite/settings.py
+++ pixel3Dsite/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ATOMIC_REQUESTS' : True
     }
 }
 
@@ -127,3 +128,4 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads/")
 MEDIA_URL = '/uploads/'
+
